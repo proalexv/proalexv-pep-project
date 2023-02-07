6cd97ba311cf9b9ac9ecd047edcc20fd880e11e0
Alejandro Rodriguez <avidleader13@gmail.com>
2023-02-07 03:35:12 +0000

    First save test; adding username to Controller

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -17,10 +21,36 @@ public class SocialMediaController {
     public Javalin startAPI() {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", registerAccountHandler);
 
         return app;
     }
+/**
+     * This is an example handler for an example endpoint.
+     * @param context The Javalin Context object manages information about both the HTTP request and response.
+     */
+    private void registerAccountHandler(Context ctx) throws JsonProcessingException {
+       // ObjectMapper mapper = new ObjectMapper();
+        //context.json("sample text");
+        ObjectMapper mapper = new ObjectMapper();
+       
+        //Is accountlclass correect?
+        Account username = mapper.readValue(ctx.body(), Account.class);
+        
+        // Add this information in the service class just like in the BookService account. 
+        Account addedAccount = addedAccount.addaccount(username);
+        if(addedAccount!=null){
+            ctx.json(mapper.writeValueAsString(addedAuthor));
+        }else{
+            ctx.status(400);
+        }
 
+        Account password = mapper.readValue(ctx.body(),Account.class);
+    }
+    }
+
+
+}
     /**
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
